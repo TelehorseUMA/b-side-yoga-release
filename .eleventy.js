81bad3422bbb0cd9f354553724c92edb4b3aa50d
fxg <fx.nippon@gmail.com>
2021-10-23 13:12:11 +0200

    add ELEVENTY_ENV via npm scripts; prettify html output with prettier in development

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,22 @@
+const path = require("path");
+const prettier = require("prettier");
+
 module.exports = function (eleventyConfig) {
+  const env = process.env.ELEVENTY_ENV;
+
+  if (env === "dev") {
+    eleventyConfig.addTransform("prettier", function (content, outputPath) {
+      const extension = path.extname(outputPath);
+      switch (extension) {
+        case ".html":
+          const parser = extension.replace(/^./, "");
+          return prettier.format(content, { parser });
+        default:
+          return content;
+      }
+    });
+  }
+
   return {
     dir: {
       input: "src",
